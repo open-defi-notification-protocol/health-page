8f2c236965e5f519ccc48b904faac73ca7fd7cc6
Degenddy <degenddy@users.noreply.github.com>
2022-06-20 19:42:03 +0300

    ui changes. + charts

---
+++ src/DoughnutChart.vue
@@ -0,0 +1,16 @@
+<script>
+import { Doughnut } from 'vue-chartjs'
+
+export default {
+  extends: Doughnut,
+  props:['chartData','options'],
+  methods: {
+    update() {
+      this.renderChart(this.chartData, this.options)
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+};
+</script>
